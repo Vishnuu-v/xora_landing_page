ea5db3e3475b1b86f8e19659ff34af2f9690929f
Vishnu V <vishnu.v.fullstack@gmail.com>
2024-09-28 13:07:27 +0530

    fixed import typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Downloads from "@/sections/Downloads";
-import FAQ from "@/sections/FAQ";
-import Features from "@/sections/Features";
-import Footer from "@/sections/Footer";
-import Header from "@/sections/Header";
-import Hero from "@/sections/Hero";
-import Pricing from "@/sections/Pricing";
-import Testimonials from "@/sections/Testimonials";
+import Downloads from "../sections/Downloads";
+import FAQ from "../sections/FAQ"
+import Features from "../sections/Features";
+import Footer from "../sections/Footer";
+import Header from "../sections/Header";
+import Hero from "../sections/Hero";
+import Pricing from "../sections/Pricing";
+import Testimonials from "../sections/Testimonials";
 
 export default function Home() {
   return (
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Link as LinkScroll } from "react-scroll";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
-    
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -44,18 +44,28 @@ const Header: React.FC = () => {
     <header
       className={clsx(
         "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
-        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
       )}
     >
       <div className="container flex h-14 items-center max-lg:px-5">
-        <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <Image src="/images/xora.svg" width={115} height={55} alt="logo" />
-        </a>
+        <div className="lg:hidden flex-1 cursor-pointer z-2">
+          <LinkScroll
+            to="hero"
+            offset={-250}
+            spy
+            smooth
+            className={clsx(
+              "min-lg:hidden transition-transform duration-500 cursor-pointer"
+            )}
+          >
+            <Image src="/images/xora.svg" width={115} height={55} alt="logo" />
+          </LinkScroll>
+        </div>
 
         <div
           className={clsx(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
-            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
@@ -74,7 +84,7 @@ const Header: React.FC = () => {
                     spy
                     smooth
                     className={clsx(
-                      "max-lg:hidden transition-transform duration-500 cursor-pointer",
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
                     )}
                   >
                     <Image
@@ -121,8 +131,8 @@ const Header: React.FC = () => {
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
             alt="magic"
             className="size-1/2 object-contain"
-            width={32} 
-            height={32} 
+            width={32}
+            height={32}
           />
         </button>
       </div>
